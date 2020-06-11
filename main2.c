43f032300be17ed76cf127c9866ccf06f001c2b2
ian <ian@localhost.localdomain>
2020-06-11 10:11:54 -0400

    added more device support

--- main2.c
+++ main2.c
@@ -37,13 +37,31 @@ void generatePairs(double *recordlist, double *pairlist, int record_count, int r
 
 void compressPairs(double *pairlist, double *parent_list, double *output_list, int record_count, int pair_count){
 
-    int i, j;
+    int i, j, l;
 
-    for(i = 0; i < record_count; i++){
-        for(j = 0; j < pair_count; j++){
+    //comparePairs(double *all_pairs, int record_count, int record_index_1, int record_index_2, int pair_index, int k, int *equal)
 
-            
+    for(l = 0; l < pair_count; l++){ //<-- stride here (by blocks)
+
+        int curr_pair = l;
+
+        for(i = 0; i < record_count; i++){
+
+            int curr_record1 = i;
+
+            for(j = i+1; j < record_count; j++){
+
+                int curr_record2 = j;
+
+                int isequal;
+
+                comparePairs(pairlist, record_count, curr_record1, curr_record2, curr_pair, pair_count, &isequal);
+
+                printf("%d", isequal);
+
+            }
         }
+
     }
 
 }
@@ -85,13 +103,18 @@ int main(int argc, char **argv){
     allocate parent list and output list
     */
 
+    //list used for all batches, keeps track of all substructures found
     double *parentlist;
     pairList_init(&parentlist, record_count, record_size, pair_size);
 
-    double *outputlist;
-    //for outputlist, pair size is 1, as it only holds index of pair in parentlist
-    pairList_init(&outputlist, record_count, record_size, 1);
+    //buffer used for holding found substructures before comparing to parentlist
+    double *parentlist_buffer;
+    pairList_init(&parentlist_buffer, record_count, record_size, pair_size);
 
+    //list of occurances of each substructure, indices align to parentlist
+    int *occurancelist;
+    occuranceList_init(&occurancelist, record_count, record_size, 1);
+    
     /*
     run compression phase 1
     */
